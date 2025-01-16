8fb2fd30a640909cce3c585ab520cd5778ac4627
Glenn Sarti <glennsarti@users.noreply.github.com>
2025-01-16 10:00:11 +0800

    Update version list (20251115)

--- features/versions_generated.go
+++ features/versions_generated.go
@@ -4,6 +4,11 @@ package features
 // DO NOT MODIFY MANUALLY
 
 var SentinelVersions = []string{
+	"v0.40.0",
+	"v0.30.0",
+	"v0.29.0",
+	"v0.28.0",
+	"v0.27.0",
 	"v0.26.3",
 	"v0.26.2",
 	"v0.26.1",
